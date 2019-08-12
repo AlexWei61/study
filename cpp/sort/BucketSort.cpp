5d35a9e872e94122f8a92e87e45093a40af55a01
Alex <alex.wei.0661@gmail.com>
2019-08-12 15:56:08 +0800

    collating documents

---
+++ cpp/sort/BucketSort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void BucketSort(int n, int num_list[]) {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		num_list[tmp]++;
+	}
+	for (int j = 0; j <= 100; j++) {
+		for (int k = 0; k < num_list[j]; k++) {
+			cout << j << " ";
+		}
+	}
+}
+
+int n;
+int num_list[101];
+
+int main() {
+	BucketSort(n, num_list);
+	return 0;
+}
